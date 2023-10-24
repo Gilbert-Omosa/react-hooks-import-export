f073037b1409181b11db098b5552e33f9124abc7
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-10-24 15:48:56 +0300

    Done with Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,17 +1,8 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-import MesaVerde from "./parks/MesaVerde";
-import * as RMFunctions from "./parks/RockyMountain";
 
-export default function ColoradoStateParks() {
-  howManyParks();
-  console.log(RMFunctions.trees);
-  RMFunctions.wildlife();
-  return ( 
-    <div>
-      <h1>Colorado State Parks!</h1>
-      <MesaVerde/>
-    </div>
-  );
+function ColoradoStateParks() {
+  howManyParks(); // => "42 parks!"
+
+  return <h1>Colorado State Parks!</h1>;
 }
-
--- src/parks/howManyParks.js
+++ src/parks/howManyParks.js
@@ -1,4 +1,3 @@
-import React from "react";
 function howManyParks() {
   console.log("42 parks!");
 }
